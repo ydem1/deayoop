6c39b9e533b2ab94dd7131022196bd619f05d630
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-03-13 20:24:09 +0200

    fix fetch

--- src/httpClient/index.ts
+++ src/httpClient/index.ts
@@ -1,16 +1,8 @@
-import axios, { AxiosResponse } from "axios";
+import axios from "axios";
 
 const BASE_URL = 'http://deayloop.backend.test.starway.agency:8002/api/';
 
-const handleResponse = (response: AxiosResponse) => {
-  if (response.status !== 200) {
-    throw new Error(`${response.status} ${response.statusText}`);
-  }
-
-  return response.data;
-};
-
 export const get = <T>(url: string): Promise<T> => (
   axios.get<T>(BASE_URL + url)
-    .then(handleResponse)
+    .then(response => response.data)
 );
