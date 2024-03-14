2dc2d42d3361166d7b94e44cc012a7c7e632ff49
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-03-14 10:39:02 +0200

    fix routes constants

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -2,9 +2,9 @@ const DOMAIN_NAME = 'deayoop';
 
 export const ROUTES = {
   home: `/${DOMAIN_NAME}/`,
-  user: `${DOMAIN_NAME}/user`,
-  userOrder: `${DOMAIN_NAME}/user/order`,
-  userFavourite: `${DOMAIN_NAME}/user/favourite`,
-  userHelp: `${DOMAIN_NAME}/user/help`,
-  userProfile: `${DOMAIN_NAME}/user/profile`,
+  user: `/${DOMAIN_NAME}/user`,
+  userOrder: `/${DOMAIN_NAME}/user/order`,
+  userFavourite: `/${DOMAIN_NAME}/user/favourite`,
+  userHelp: `/${DOMAIN_NAME}/user/help`,
+  userProfile: `/${DOMAIN_NAME}/user/profile`,
 };
